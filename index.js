3c82fc53a36220f32524f67a64b9d642f99f66b7
Divyansh <divi.sunway@gmail.com>
2020-08-24 18:38:10 +0530

    remove everything except answer

---
+++ index.js
@@ -0,0 +1,10 @@
+
+$('.exp_content.maxHeight75px').css("all","unset");
+
+var foot= $('#footer')
+foot.remove()
+var sideBox = $('#sidebar')
+sideBox.remove()
+var ansDekho = $('.full-answer-link')
+ansDekho.remove()
+
